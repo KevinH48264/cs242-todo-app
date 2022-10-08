b24097ff74587c51529a1e5f5ec59c13cb07b693
KevinH48264 <1kevin.huang@gmail.com>
2022-10-07 21:22:23 -0400

    flask app commented!

---
+++ todo-flask/app.py
@@ -0,0 +1,54 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+# define project directory so that we can access the database
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
+DATABASE = os.path.join(PROJECT_ROOT, 'todo.db')
+
+# instantiate app and database
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + DATABASE
+db = SQLAlchemy(app)
+
+# instantiate class Todo for our model
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    complete = db.Column(db.Boolean)
+
+# this is the index page -- query all Todos
+@app.route("/")
+def index():
+    todo_list = Todo.query.all()
+    return render_template("index.html", todo_list=todo_list)
+
+# this is our add route that adds a new todo and redirects the user back to index page
+@app.route("/add", methods=["POST"])
+def add():
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+# this is a completed route that toggles the todo's completion status
+@app.route("/complete/<string:todo_id>")
+def complete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("index"))
+
+# this is a delete route that deletes the todo and saves it in the database
+@app.route("/delete/<string:todo_id>")
+def delete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+# this is what happens when the script is run -- it creates the database and runs the app
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug=True)
---
+++ todo-flask/readme.md
@@ -0,0 +1,14 @@
+# Todo App Clone with Flask
+
+## How to run
+
+1. Git clone this repository
+2. Run 'pip install -r requirements.txt' in terminal
+3. Run app.py in terminal
+4. Go to http://127.0.0.1:5000/ and check it out!
+
+What are the significant software concepts that this combination of technologies has that each previous set of technologies did not? Or that they handle significantly differently?
+
+Flask was an extremely lightweight framework that I've used in the past. It is a python library that doesn't require the use of an external database hosting site (for development purposes) unlike Firebase and MongoDB. However, this means that your data cannot persist unless connected to a third party database hosting site. This combination of technology also had the concept of routes which changed the url, but still allowed for GET and POST commands. The routes were different from any other technology, but it allowed for easy use and makes clear what are the different functions and pages in the app, from the point of view of a developer.
+
+Commented from: https://github.com/onurtacc/flask-todo-app
